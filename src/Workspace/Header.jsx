e5988d16a2a0ae57ba0ad30a6f414ef70d82304b
thirupathi8 <124600966+thirupathi8@users.noreply.github.com>
2025-01-02 13:28:15 +0530

    Update Header.jsx

--- src/Workspace/Header.jsx
+++ src/Workspace/Header.jsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/clerk-react'
 const Header = () => {
     return (
         <div className='flex justify-between shadow-md p-5'>
-            <img src="src\assets\logo.svg" alt="logo" width={120} height={120} />
+            <img src="/logo.svg" alt="logo" width={120} height={120} />
             <UserButton />
         </div>
     )
